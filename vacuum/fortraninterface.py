d928eceaaa33a1d797e7c99815f2f8e0ffab146f
liuk2020 <lk2020@mail.ustc.edu.cn>
2024-11-12 14:43:00 +0800

    change data type of basefunction

--- vacuum/fortraninterface.py
+++ vacuum/fortraninterface.py
@@ -9,15 +9,15 @@ from typing import Tuple
 
 
 # basefunction.f90
-def get_zernike(r: np.ndarray, lrad: int, mpol: int) -> np.ndarray:
+def get_zernike(r: float, lrad: int, mpol: int) -> np.ndarray:
     return vacuum_fortran.get_zernike(r, lrad, mpol)
 
 
-def get_zernike_d2(r: np.ndarray, lrad: int, mpol: int) -> np.ndarray:
+def get_zernike_d2(r: float, lrad: int, mpol: int) -> np.ndarray:
     return vacuum_fortran.get_zernike_d2(r, lrad, mpol)
 
 
-def get_zernike_rm(r: np.ndarray, lrad: int, mpol: int) -> np.ndarray:
+def get_zernike_rm(r: float, lrad: int, mpol: int) -> np.ndarray:
     return vacuum_fortran.get_zernike_rm(r, lrad, mpol)
 
 
